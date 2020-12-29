a2682b710dcb919b9d794c46f963075131ebab32
Gyeong <kamniang@naver.com>
2020-12-29 18:35:16 +0900

    node server practice

---
+++ common/models/address.js
@@ -0,0 +1,103 @@
+'use strict';
+const logger = require('../../server/logger').slog;
+
+module.exports = function(Address) {
+	//createAddressByUserId
+	Address.createAddressByUserId = (mainAddress, subAddress, zipCode, seq, isMain, req, cb) => {
+		Address.create({
+			mainAddress: mainAddress,
+			subAddress: subAddress,
+			zipCode: zipCode,
+			seq: seq,
+			isMain: isMain,
+			userId: req.userInfo.id.toString()
+		})
+		.then(result => {
+			return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error)
+			return cb(error)
+		});
+	};
+
+	Address.findAll = (cb) => {
+		Address.find({})
+		.then(result => {
+		return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error)
+			return cb(error)
+		})
+	}
+
+//토큰에 있는 유저아이디를 조건으로
+	Address.me = (req, cb) => {
+		Address.find({
+			where: {
+				userId: req.userInfo.id
+			},
+			order: 'seq ASC'
+		})
+		.then(result => {
+			return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error);
+			return cb(error)
+		})
+	};
+
+	Address.deleteById = (id, req, cb) => {
+		Address.findOne({
+			where:{
+				and:[
+					{userId: req.userInfo.id.toString()},
+					{id: id}
+				]
+			}
+		})
+		.then(address => {
+			if(!address) {
+				throw new Error('에러에러');
+			}
+			return address.destroy()
+		})
+		.then(result => {
+			console.log('result', result);
+			return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error);
+			return cb(error)
+		})
+	};
+
+	Address.updateAddress = (mainAddress, req, cb) => {
+		Address.updateAll({
+			where: {
+				userId: req.userInfo.id.toString()
+			},
+			mainAddress: mainAddress
+		})
+		.then(result => {
+			return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error);
+			return cb(error)
+		})
+	};
+
+	Address.updateById = (id, data, req, cb) => {
+		Address.updateAll({id:id}, { ...data })
+		.then(result => {
+			return cb(null, result);
+		})
+		.catch(error => {
+			logger.error(error);
+			return cb(error)
+		})
+	};
+};
---
+++ common/models/user.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const logger = require('../../server/logger').slog;
+
+module.exports = function(User) {
+
+  User.observe('before save', (context, next) => {
+    const _instance = context.instance;
+    const updatedDate = new Date();
+
+    if (_instance) {
+      _instance.emailVerified = true;
+      _instance.created = updatedDate;
+      _instance.updated = updatedDate;
+    } else {
+      if (!context.currentInstance) {
+        return next();
+      } else {
+        context.data.updated = updatedDate;
+      }
+    }
+
+    return next();
+  });
+
+	User.createUserByParams = (params, cb) => {
+		User.create({...params})
+		.then(result => {
+			return cb(null, result);
+		})
+		.catch(error => {
+			return cb(error);
+		});
+	}
+
+  User.me = (req, cb) => {
+    logger.info("여기가 프로필을 요청하는 API");
+    const _user = req.userInfo;
+
+    delete _user.password;
+
+    logger.info("response Data:" , _user);
+    return cb(null, _user);
+  }; 
+};
+
+//promise 문법
+//JS는 기본적으로 함수형 언어, 비동기가 기본
+//동기 처리를 하기 위한 문법
+
+//callback이 불러주는 순간 api가 끝난다... -> 뒤의 로직이 남아있어도 프론트에 전달되지 않는다
+//callback을 중복해서 부르면 에러가 난다
